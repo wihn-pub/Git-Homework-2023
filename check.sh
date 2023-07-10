00ce4f3ab872fcebe8cc983c8611a698d902a93b
wihn <wihn2021@163.com>
2023-07-10 23:20:16 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# 读取info.txt中的三行文字
-read line1
-read line2
-read line3 < info.txt
+content=$(cat ./hw.txt)
+lines=(0 0 0)
+i=0
 
-# 使用sed命令提取每行末尾的数字
-num1=$(echo "$line1" | sed 's/.*\([0-9]\)$/\1/')
-num2=$(echo "$line2" | sed 's/.*\([0-9]\)$/\1/')
-num3=$(echo "$line3" | sed 's/.*\([0-9]\)$/\1/')
+for line in $content
+do
+    lines[$i]=$line
+    i=$((i+1))
+done
 
-# 计算并判断结果
-result=$(expr $num1 \* $num2 % 10)
-if [ $result -eq $num3 ]; then
-  echo "第一行和第二行末尾的数字乘积的个位为$num3"
+ans=$((${lines[0]: -1} * ${lines[1]: -1}))
+res=$(echo ${lines[2]} | tr -cd "[0-9]")
+# echo $ans
+# echo $res
+
+if [ $ans != $res ]
+then
+    exit 1
 else
-  echo "第一行和第二行末尾的数字乘积的个位不为$num3"
-  exit 1 # 执行失败
+    echo "correct"
 fi
